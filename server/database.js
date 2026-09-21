c29f0d4fa28971ff08538ac2c1c24c9ff13b872d
konatardanilo888-spec <konatardanilo888@gmail.com>
2026-09-21 19:10:11 +0200

    Update database.js

--- server/database.js
+++ server/database.js
@@ -18,7 +18,7 @@ const config = require('./config');
 // Osiguraj da folder za bazu postoji.
 const dbDir = path.dirname(config.databasePath);
 if (!fs.existsSync(dbDir)) {
-  fs.mkdirSync(dbDir, { recursive: true });
+    fs.mkdirSync(dbDir, { recursive: true });
 }
 
 const db = new DatabaseSync(config.databasePath);
@@ -30,10 +30,30 @@ db.exec('PRAGMA journal_mode = WAL;');
  * database/schema.sql.
  */
 function initSchema() {
-  const schemaSql = fs.readFileSync(config.schemaPath, 'utf8');
-  db.exec(schemaSql);
+    const schemaSql = fs.readFileSync(config.schemaPath, 'utf8');
+    db.exec(schemaSql);
+}
+
+/**
+ * Dodaje kolone koje su uvedene poslije prve verzije šeme, za baze koje
+ * već postoje na trajnom disku (CREATE TABLE IF NOT EXISTS ih ne bi dodao).
+ * Svaki ALTER je zaštićen try/catch - ako kolona već postoji, greška se
+ * jednostavno ignoriše.
+ */
+function addColumnIfMissing(table, column, definition) {
+    try {
+          db.exec(`ALTER TABLE ${table} ADD COLUMN ${column} ${definition}`);
+    } catch (err) {
+          if (!/duplicate column name/i.test(err.message)) throw err;
+    }
+}
+
+function runMigrations() {
+    addColumnIfMissing('notifications', 'title_en', 'TEXT');
+    addColumnIfMissing('notifications', 'content_en', 'TEXT');
 }
 
 initSchema();
+runMigrations();
 
 module.exports = db;
